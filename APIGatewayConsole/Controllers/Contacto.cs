d60a8e321b7e6e32aa24fa527ad5044001c44de1
Alvaro <alvaro@eliminalia.com>
2019-05-15 17:38:58 +0200

    APIGateway modo consola.

---
+++ APIGatewayConsole/Controllers/Contacto.cs
@@ -0,0 +1,65 @@
+﻿using Grapevine.Interfaces.Server;
+using Grapevine.Server;
+using Grapevine.Server.Attributes;
+using Grapevine.Shared;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIGatewayConsole
+{
+    [RestResource]
+    public class TestResource
+    {
+
+        #region GET
+        [RestRoute(HttpMethod = HttpMethod.GET, PathInfo = "/api/contacto/all")]
+        public IHttpContext ReadAllContacto(IHttpContext context)
+        {
+            
+            var runer = Task.Run(() =>
+            {
+                LibCore.Start _ = new LibCore.Start("");
+                var dataRun = _.Contacto.ReadAllContacto();
+                return JsonConvert.SerializeObject(dataRun, Formatting.Indented);
+            });
+
+            runer.Wait();
+            runer.Dispose();
+
+            context.Response.AppendHeader("Content-Type", "application/json");
+            context.Response.SendResponse(runer.Result);
+            return context;
+        }
+
+        [RestRoute(HttpMethod = HttpMethod.GET, PathInfo = "/api/contacto/one")]
+        public IHttpContext ReadOneContacto(IHttpContext context)
+        {
+
+            var runner = Task.Run(() => {
+                LibCore.Start _ = new LibCore.Start("");
+                var id = context.Request.QueryString["id"] ?? "noneid?"; //Si no id dara error
+                var dataRun = _.Contacto.ReadOneContacto(id);
+                return JsonConvert.SerializeObject(dataRun, Formatting.Indented);
+            });
+
+            runner.Wait();
+            runner.Dispose();
+
+            context.Response.AppendHeader("Content-Type", "application/json");
+            context.Response.SendResponse(runner.Result);
+            return context;
+        }
+        #endregion
+
+        [RestRoute]
+        public IHttpContext Default(IHttpContext context)
+        {
+            context.Response.SendResponse("APIGateway.");
+            return context;
+        }
+    }
+}
---
+++ APIGatewayConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using Grapevine.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIGatewayConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "APIGateway v0.1.0.0";
+            Console.WriteLine("     A P I   G A T E W A Y   v0.1.0.0 ");
+            Console.ReadLine();
+
+            using (var server = new RestServer())
+            {
+                server.Port = "5000";
+                server.Host = "*";
+                server.LogToConsole().Start();
+                Console.Title = "[ON] APIGateway v0.1.0.0";
+                Console.ReadLine();
+                server.Stop();
+                System.GC.Collect();
+                
+            }
+        Console.ReadLine();
+        }
+    }
+
+
+}
